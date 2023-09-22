1ec26048479b818652146622f3f6570c3b6c8fcf
robbie reta <robbie.ret67@gmail.com>
2023-09-22 16:35:27 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import { useState, useEffect } from "react";
-import lista from "./lista.json";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import { MDBDataTable } from "mdbreact";
+import axios from "axios";
 
-function App() {
+
+
+func
+
+tion App() {
   //save axios  response to state
-  let [data2, setData] = useState([]);
+  let [data, setData] = useState([]);
+
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://a-d9z9.onrender.com/precios',
+    headers: { }
+  };
+  
+  axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+    let lista=response.data; 
+    
+ 
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+  
+
 
   // The useEffect() hook fires any time that the component is rendered.
   // An empty array is passed as the second argument so that the effect only fires once.
@@ -18,7 +42,7 @@ function App() {
 
 
   
-  const data = {
+  const cols = {
     columns: [
       {
         label: "Codigo",
@@ -45,13 +69,13 @@ function App() {
         width: 150,
       }
     ],
-    rows: data2.lista,
+    rows: data.lista,
   };
 
   return (
     <div className="App">
       <h1>Mastermoto</h1>
-      <MDBDataTable striped bordered hover data={data} />
+      <MDBDataTable striped bordered hover data={cols} />
     </div>
   );
 }
